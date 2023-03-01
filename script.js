ab4c53106dd005da141e047c65e323652a4b666e
anjila0 <anjila@codepixelzmedia.com.np>
2023-03-01 18:04:23 +0545

    testimonial slider
    I worked in a testimonial slider today . And also in FAQS Accordion from bootstrap 5.3 alpha version .

--- script.js
+++ script.js
@@ -5,4 +5,68 @@ $(".slider").slick({
   slidesToShow: 1,
   centerMode: true,
   variableWidth: true,
+
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
 });
+
+$(".testimonial-content").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+// FAQS section
+
+const accordion = document.getElementsByClassName("container");
+
+// for (i = 0; i < accordion.length; i++) {
+//   accordion[i].addEventListener("click", function () {
+//     this.classList.toggle("active");
+//   });
+// }
